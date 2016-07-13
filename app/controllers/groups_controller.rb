aa83a7a247e4399afe010e29e90846af77daa516
ed kerry <edwardkerry@notonthehighstreet.com>
2016-07-13 17:21:50 +0100

    Adds devise controllers WIP

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,6 +10,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     @group.user_id = current_user.id
     @group.save
     redirect_to '/'
@@ -18,6 +19,9 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
+
+
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :groups
 
   root to: "home#index"
+
+  # devise_for :users, controllers: {
+  #     sessions: 'users/sessions'
+  #   }
 end
